2e263450c9a452858ea490562cd6c415847d2f70
Richard Bryant <rich.bryant@live.co.uk>
2019-11-27 13:24:04 +0000

    Added data classes

---
+++ Instem.Movies/Data/Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Instem.Movies.Data
+{
+    public class Info
+    {
+        public string[] Directors { get; set; }
+        
+        public DateTime ReleaseDate { get; set; }
+        
+        public double Rating { get; set; }
+
+        public string[] Genres { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Plot { get; set; }
+
+        public int Rank { get; set; }
+
+        public int RunningTimeSecs { get; set; }
+
+        public string[] Actors { get; set; }
+    }
+}
---
+++ Instem.Movies/Data/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Instem.Movies.Data
+{
+    public class Movie
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public Info Info { get; set; }
+    }
+}
