6ad4d8b8ee6856040cd35213a709385b4320420e
Richard Bryant <rich.bryant@live.co.uk>
2019-11-27 13:46:50 +0000

    Red test - DataLoader

--- Instem.Movies.Tests/MovieServiceTests.cs
+++ Instem.Movies.Tests/MovieServiceTests.cs
@@ -1,10 +1,21 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using FluentAssertions;
 
 namespace Instem.Movies.Tests
 {
     [TestFixture]
     public class MovieServiceTests
     {
-        
+        [Test]
+        public void LoadDataFromJsonFileTest()
+        {
+            var dataLocation = @"DataLocation";
+            var loaderUnderTest = new DataLoader();
+            var loadedData = loaderUnderTest.Load(dataLocation);
+
+            loadedData.Should().NotBeEmpty();
+
+        }
     }
 }
