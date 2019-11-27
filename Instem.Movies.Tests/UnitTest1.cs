812e1a47c59003de96a41afffa5170e9428cfc39
Richard Bryant <rich.bryant@live.co.uk>
2019-11-27 13:11:05 +0000

    Added tests project

---
+++ Instem.Movies.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Instem.Movies.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
